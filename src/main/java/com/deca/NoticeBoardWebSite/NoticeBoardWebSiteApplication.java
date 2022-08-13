c9015cc404a658949c343fa6e69601f2086e3612
JeongHunHui <deca3248@gmail.com>
2022-08-13 16:03:23 +0900

    set: spring + react 개발환경 세팅 완료

---
+++ src/main/java/com/deca/NoticeBoardWebSite/NoticeBoardWebSiteApplication.java
@@ -0,0 +1,13 @@
+package com.deca.NoticeBoardWebSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeBoardWebSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticeBoardWebSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deca/NoticeBoardWebSite/NoticeBoardWebSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deca.NoticeBoardWebSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticeBoardWebSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
