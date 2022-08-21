6632901bec478f1c62b702c7cb69cbfdf2ab775e
JeongHunHui <deca3248@gmail.com>
2022-08-22 00:02:56 +0900

    feat: comment repository 구현

--- src/main/java/com/deca/NoticeBoardWebSite/repository/CommentRepository.java
+++ src/main/java/com/deca/NoticeBoardWebSite/repository/CommentRepository.java
@@ -1,4 +1,11 @@
 package com.deca.NoticeBoardWebSite.repository;
 
+import com.deca.NoticeBoardWebSite.domain.Comment;
+
+import java.util.List;
+
 public interface CommentRepository {
+    void save(Comment comment); // 댓글 저장
+    List<Comment> findById(Long postId); // 게시물 id로 댓글 불러오기
+    Long getCommentCountById(Long postId); // 게시물 id로 댓글 개수 불러오기
 }
--- src/main/java/com/deca/NoticeBoardWebSite/repository/JpaCommentRepository.java
+++ src/main/java/com/deca/NoticeBoardWebSite/repository/JpaCommentRepository.java
@@ -1,4 +1,35 @@
 package com.deca.NoticeBoardWebSite.repository;
 
-public class JpaCommentRepository {
+import com.deca.NoticeBoardWebSite.domain.Comment;
+import com.deca.NoticeBoardWebSite.domain.PostData;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+public class JpaCommentRepository implements CommentRepository{
+
+    private final EntityManager em;
+
+    public JpaCommentRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    @Override
+    public void save(Comment comment) {
+        em.persist(comment);
+    }
+
+    @Override
+    public List<Comment> findById(Long postId) {
+        List<Comment> list = em.createQuery("select p from comment p where p.postId = :postId", Comment.class)
+                .setParameter("postId", postId)
+                .getResultList();
+        return list;
+    }
+
+    @Override
+    public Long getCommentCountById(Long postId) {
+        return Long.valueOf(findById(postId).size());
+    }
 }
